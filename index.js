995349a6a31ef5963085d6ca3a12faf306d5dcba
Cory Boyd <corytheboyd@gmail.com>
2016-03-23 15:45:11 -0700

    React and Redux boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hay');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Game from './components/Game'
+import game from './reducers'
+
+const store = createStore(game)
+const rootElement = document.getElementById('root')
+
+function render () {
+  ReactDOM.render(
+    <Game />,
+    rootElement
+  )
+}
+
+render()
+store.subscribe(render)
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
   output: {
     path: __dirname + "/public",
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel",
+        query: {
+          presets: ["react", "es2015"]
+        }
+      }
+    ]
   }
-};
+}
