ea8aa247a809475c04fae9550843bd347377354b
Cory Boyd <corytheboyd@gmail.com>
2016-03-24 11:56:24 -0700

    Add stylesheet

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Game from './components/Game'
 
+require('./style.css')
+
 const rootElement = document.createElement('div')
 rootElement.setAttribute('id', 'root')
 document.body.appendChild(rootElement)
